9ed0670d79f243f3d11f2679e8bc4f706cf7b118
Andrew Rasmussen <andyras@gmail.com>
2013-08-28 13:18:19 -0500

    autotune written but not working, something is up with initial layout

--- deterriblesquareify.py
+++ deterriblesquareify.py
@@ -2,6 +2,8 @@
 
 import wx
 
+debug = True
+
 class Square(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, title='Deterriblesquareify')
@@ -62,7 +64,7 @@ class Square(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.onLoad, loadBtn)
         self.Bind(wx.EVT_BUTTON, self.onSave, saveBtn)
         self.Bind(wx.EVT_BUTTON, self.onReset, resetBtn)
-        self.Bind(wx.EVT_BUTTON, self.onAuto, autoBtn)
+        self.Bind(wx.EVT_BUTTON, self.onReset, autoBtn)
 
         self.Bind(wx.EVT_SIZE, self.onSize)
 
@@ -274,10 +276,137 @@ class Square(wx.Frame):
             self.panel.Refresh()
 
     def onAuto(self, e=None):
+        '''
+        Automatically finds the broken quadrant and adjusts it to match its neighbors.
+        '''
+
         print 'Autotuning...'
 
+        xDim = self.currentImg.GetSize()[0]
+        yDim = self.currentImg.GetSize()[1]
+
+        if (debug):
+            print 'xDim '+str(xDim)
+            print 'yDim '+str(yDim)
+
+        ## construct vectors of grayscale values on adjacent quadrant edges
+        #TODO read grayscale values as matrix
+
+        # bottom of Q1 and top of Q4
+        q1b = []
+        q4t = []
+        for x in range(xDim/2, xDim):
+            q1b.append(self.currentImg.GetRed(x, yDim/2 - 1))
+            q4t.append(self.currentImg.GetRed(x, yDim/2))
+
+        # left of Q1 and right of Q2
+        q1l = []
+        q2r = []
+        for y in range(0, yDim/2):
+            q1l.append(self.currentImg.GetRed(xDim/2, y))
+            q2r.append(self.currentImg.GetRed(xDim/2 - 1, y))
+
+        # bottom of Q2 and top of Q3
+        q2b = []
+        q3t = []
+        for x in range(0, xDim/2):
+            q2b.append(self.currentImg.GetRed(x, yDim/2 - 1))
+            q3t.append(self.currentImg.GetRed(x, yDim/2))
+
+        # right of Q3 and left of Q4
+        q3r = []
+        q4l = []
+        for y in range(yDim/2, yDim):
+            q3r.append(self.currentImg.GetRed(xDim/2 - 1, y))
+            q4l.append(self.currentImg.GetRed(xDim/2, y))
+
+        ## find which quadrant is most likely to be the maladjusted one
+        # calculate rms difference for each edge
+        rmsRight = self.rms(q1b, q4t)
+        rmsTop = self.rms(q1l, q2r)
+        rmsLeft = self.rms(q2b, q3t)
+        rmsBottom = self.rms(q3r, q4l)
+
+        # each quadrant has two edges
+        print rmsTop
+        rmsQ1 = rmsTop + rmsRight
+        rmsQ2 = rmsTop + rmsLeft
+        rmsQ3 = rmsLeft + rmsBottom
+        rmsQ4 = rmsBottom + rmsRight
+
+        # find the largest
+        if (max(rmsQ1, rmsQ2, rmsQ3, rmsQ4) == rmsQ1):
+            q = 1
+            insideEdge = q1l + q1b
+            outsideEdge = q2r + q4t
+        elif (max(rmsQ1, rmsQ2, rmsQ3, rmsQ4) == rmsQ2):
+            q = 2
+            insideEdge = q2r + q2b
+            outsideEdge = q1l + q3t
+        elif (max(rmsQ1, rmsQ2, rmsQ3, rmsQ4) == rmsQ3):
+            q = 3
+            insideEdge = q3r + q3t
+            outsideEdge = q4l + q2b
+        else:
+            q = 4
+            insideEdge = q4l + q4t
+            outsideEdge = q3r + q1b
+
+        if (debug):
+            print 'Inside Edge:'
+            print insideEdge
+            print 'Outside Edge:'
+            print outsideEdge
+
+        ## find n which minimizes the rms difference in grayscale intensity along edges of quadrant
+        n = 0
+        oldRMSValue = self.rms(insideEdge, outsideEdge)
+
+        nSign = -1
+        # case where pixels in quadrant need to be adjusted up
+        if (sum(insideEdge) < sum(outsideEdge)):
+            nSign = 1
+
+        iter = 0
+        maxIter = 255
+        newRMSValue = 0
+        while ((iter < maxIter) and (newRMSValue < oldRMSValue)):
+            # increase the adjustment to the insideEdge pixels
+            n += nSign
+            # don't reset oldRMSValue on the first iteration
+            if (iter > 0):
+                oldRMSValue = newRMSValue
+            # find new adjusted RMS value
+            newRMSValue = self.rmsAdjust(insideEdge, outsideEdge, n)
+            iter += 1
+
+        ## adjust the quadrant
+        self.adjustQuadrant(q, n)
+
+    def rms(self, v1, v2):
+        return self.rmsAdjust(v1, v2, 0)
+
+    def rmsAdjust(self, v1, v2, n):
+        '''
+        Finds the RMS difference between two vectors, with the values in the
+        first vector adjusted by n.
+        '''
+
+        if (debug):
+            print 'v1:'
+            #print v1
+            print 'v2:'
+            #print v2
+
+        squareSum = 0.0
+        for i in range(len(v1)):
+            squareSum += (v1[i] - v2[i])**2
+
+        print (squareSum/len(v1))**0.5
+        return (squareSum/len(v1))**0.5
+
     def onSize(self, e=None):
-        self.imgSize = self.imgSizer.GetSize()[1]
+        self.imgSize = self.imgSizer.GetSize()[0]
         print 'Height of image sizer is '+str(self.imgSize)
         self.displayImg = self.currentImg.Scale(self.imgSize, self.imgSize)
         self.myImg.SetBitmap(wx.BitmapFromImage(self.displayImg))
